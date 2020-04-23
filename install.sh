89ef9d521f542421d1421626712a93e80e9094a4
Coty Condry <ccondry@cisco.com>
2020-04-23 16:43:22 -0500

    don't copy certs folder or toolbox cert during install script

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@ npm i
 if [ $? -eq 0 ]; then
   echo "edit .env file first"
   vim .env
-  echo "creating certs directory"
-  mkdir ./certs
-  echo "copying toolbox public cert from /etc/ssl/certs to local folder"
-  cp /etc/ssl/certs/toolbox.pem ./certs/rsa-public.pem
+  #echo "creating certs directory"
+  #mkdir ./certs
+  #echo "copying toolbox public cert from /etc/ssl/certs to local folder"
+  #cp /etc/ssl/certs/toolbox.pem ./certs/rsa-public.pem
   echo "installing systemd service..."
   sudo cp systemd.service /lib/systemd/system/wxm-toolbox-api.service
   sudo systemctl enable wxm-toolbox-api.service
