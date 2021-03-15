d08f7631a0cbc71f3485c76b4daf7f6a75a51c1d
Coty Condry <ccondry@cisco.com>
2021-03-15 14:58:09 -0500

    disable heal vertical

--- src/models/verticals/index.js
+++ src/models/verticals/index.js
@@ -1,11 +1,11 @@
 const bank = require('./bank')
-const heal = require('./heal')
+// const heal = require('./heal')
 const product = require('./product')
 const retail = require('./retail')
 
 module.exports = {
   bank,
-  heal,
+  // heal,
   product,
   retail
 }
